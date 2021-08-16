8597b31cd7604075c011df17de841f367c698168
grant amaral <grantamaral@gmail.com>
2021-08-16 07:02:07 -0600

    Update boi2json.php
    removed some commenting

--- boi2json.php
+++ boi2json.php
@@ -12,13 +12,6 @@ $gid =41;
 $agency = "%usgs%";
 $station = 13206000;
 $result = array();
-$sql = <<<SQL
-SELECT gages.cfs, gages.gid, gages.name, 
-  idaho.low, idaho.high, idaho.gid, idaho.section 
-  FROM gages 
-  INNER JOIN idaho ON gages.gid = idaho.gid 
-  WHERE section LIKE ? 
-SQL;
 
 $sql = <<<SQL
 SELECT android.id, android.name, android.imageurl, gages.cfs, gages.usgs, 
@@ -27,7 +20,6 @@ MONTH( android.updated ) AS month,
 TIME_FORMAT(`updated`, '%H:%i') as hour,
 android.agency
 FROM rivers.android 
-
 INNER JOIN rivers.gages
 ON gages.usgs = android.usgs
 WHERE gages.usgs = ?
@@ -35,15 +27,13 @@ and android.agency LIKE ?
 SQL;
 
 $stmt = $pdo->prepare($sql);
-// $stmt->execute([$section]);
 $stmt->execute([$station, $agency]);
 $data = $stmt->fetchAll();
 
-
 foreach($data as $result) {
-    // echo $result['cfs'] . ' ' . $result['lng'] . '<br>';
+    // echo $result['cfs'] . ' ' . $result['agency'] . '<br>';
 }
-//print $json = (json_encode(['gages' => $result]));
+
 print $json = (json_encode($result, JSON_NUMERIC_CHECK));
 // this php file can serve as a json file though it might give a srict mime error
 file_put_contents ('boise.json', $json)
